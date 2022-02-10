0c6a831ef70e94667516b82f4ef61d1f35eaa462
shyrapson <meetshyrapson@gmail.com>
2022-02-10 23:19:27 +0100

    comit

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Select, Typography, Row, Col, Avatar, card} from 'antd'
-
+import moment from 'moment';
+import { useGetCryptosQuery } from '../services/cryptoApi';
+import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 function News() {
   return <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, rem.</div>;
 }
--- src/services/cryptoNewsApi.jsx
+++ src/services/cryptoNewsApi.jsx
@@ -1,22 +1,15 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-
-
-// const cryptoApiHeaders = {
-//   'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-//   'x-rapidapi-key': '24afd95508msh2c343f20c75ba19p100ab6jsnca13a0510852'
-// };
-const baseUrl = 'https://coinranking1.p.rapidapi.com';
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
 const createRequest = (url) => ({  
-method: 'GET',
-url,
-params: {
-
-},
-headers: {
-
-}
-
+    method: 'GET',
+    url,
+    params: {safeSearch: 'Off', textFormat: 'Raw'},
+    headers: {
+      'x-bingapis-sdk': 'true',
+      'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+      'x-rapidapi-key': '24afd95508msh2c343f20c75ba19p100ab6jsnca13a0510852'
+    }
 
 });
 
@@ -25,10 +18,11 @@ export const cryptoNewsApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getCryptos: builder.query({
-      query: (count) => createRequest(`/coins?limit=${count}`),
+      query: ({newsCategory,count}) =>createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
     }),
   }),
 });
 
 // this will be imported in the component that it is needed
 export const { useGetCryptosNewsQuery } = cryptoNewsApi;
+
