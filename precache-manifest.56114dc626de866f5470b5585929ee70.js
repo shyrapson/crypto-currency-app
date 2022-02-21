0eef910e4a201b4a219735c08091e5b2f43080ef
shyrapson <meetshyrapson@gmail.com>
2022-02-21 16:51:25 +0100

    Updates

---
+++ precache-manifest.56114dc626de866f5470b5585929ee70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5869a26d2effe29b648457b11b97350a",
+    "url": "/crypto-currency-app/index.html"
+  },
+  {
+    "revision": "6e5fa7ef5adc3a538e38",
+    "url": "/crypto-currency-app/static/css/2.cf6f5237.chunk.css"
+  },
+  {
+    "revision": "91d94467a6e9fe244f80",
+    "url": "/crypto-currency-app/static/css/main.4bc2b983.chunk.css"
+  },
+  {
+    "revision": "6e5fa7ef5adc3a538e38",
+    "url": "/crypto-currency-app/static/js/2.f15e107d.chunk.js"
+  },
+  {
+    "revision": "91d94467a6e9fe244f80",
+    "url": "/crypto-currency-app/static/js/main.cb69da47.chunk.js"
+  },
+  {
+    "revision": "c32c18c9f4d7db1ccfa5",
+    "url": "/crypto-currency-app/static/js/runtime~main.2937390b.js"
+  },
+  {
+    "revision": "885327321794d77a6b6f7ab7fd42d271",
+    "url": "/crypto-currency-app/static/media/cryptocurrency.88532732.png"
+  }
+]);
